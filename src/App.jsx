8f053c57f500d96b0d0a2806fd337c1b938f44af
David Diaz <davidcol123dm@gmail.com>
2023-02-28 21:15:58 -0500

    secont commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import { useState } from 'react';
 
 import users from './assets/json/users.json';
 import { getRandomNumber as random } from './assets/utils/getRandomNumber';
+import UserCard from './components/UserCard';
 
 const App = () => {
   const [userIndex, setUserIndex] = useState(random(0, users.length - 1));
   const user = users[userIndex];
 
   const changeUser = () => {
-    let newUser=useState(random(0, users.length - 1);
-    while (newUser === userIndex) {
-      
-    }
+    let newUser = useState(random(0, users.length - 1));
+    while (newUser === userIndex) {}
   };
 
   return (
     <div className="h-full flex flex-col justify-center items-center p-10">
-      
+      <UserCard user={user}/>
+      <button>a</button>
     </div>
   );
 };
